ca992d691cc52c372a334958a460cd17afe99fde
Diego Hartwig <hartwig.diego@gmail.com>
2018-10-02 18:07:03 -0300

    Implementacao metodos para deletar e atualizar os users

--- src/main/java/br/net/hartwig/springmongodb/services/UserService.java
+++ src/main/java/br/net/hartwig/springmongodb/services/UserService.java
@@ -37,6 +37,27 @@ public class UserService {
 
 	}
 
+	public void delete(String id) {		
+
+		repo.deleteById(id);
+	}
+
+	public User update(User user) {
+
+		User newUser = findById(user.getId());
+
+		updateData(newUser, user);
+
+		return repo.save(newUser);
+	}
+
+	private void updateData(User newUser, User user) {
+
+		newUser.setName(user.getName());
+		newUser.setEmail(user.getEmail());
+
+	}
+
 	public User fromDTO(UserDTO userDto) {
 		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
