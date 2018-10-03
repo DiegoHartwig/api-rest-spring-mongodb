4bfdb3796567af9195b752c643709f0c0e188a28
Diego Hartwig <hartwig.diego@gmail.com>
2018-10-02 21:47:36 -0300

    Refatoracao do post e AuthorDTO

--- src/main/java/br/net/hartwig/springmongodb/config/Instantiation.java
+++ src/main/java/br/net/hartwig/springmongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import br.net.hartwig.springmongodb.domain.Post;
 import br.net.hartwig.springmongodb.domain.User;
+import br.net.hartwig.springmongodb.dto.AuthorDTO;
 import br.net.hartwig.springmongodb.repository.PostRepository;
 import br.net.hartwig.springmongodb.repository.UserRepository;
 
@@ -36,12 +37,12 @@ public class Instantiation implements CommandLineRunner {
 		User diego = new User(null, "Diego Hartwig", "hartwig.diego@gmail.com");
 		User aline = new User(null, "Aline Cardoso", "acardoso@gmail.com");
 
-		Post post1 = new Post(null, simpleDateFormat.parse("02/10/2018"), "Primeiro post",
-				"Esse é o primeiro post de teste", diego);
-		Post post2 = new Post(null, simpleDateFormat.parse("02/10/2018"), "Segundo post",
-				"Esse é o segundo post de teste", aline);
-
 		userRepository.saveAll(Arrays.asList(diego, aline));
+
+		Post post1 = new Post(null, simpleDateFormat.parse("02/10/2018"), "Primeiro post",
+				"Esse é o primeiro post de teste", new AuthorDTO(diego));
+		Post post2 = new Post(null, simpleDateFormat.parse("02/10/2018"), "Segundo post",
+				"Esse é o segundo post de teste", new AuthorDTO(aline));
 
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
